5b2a89438a06b5ae2449413d85f31afedf0e5906
damon <915897364@qq.com>
2024-01-29 11:42:30 +0800

    异步

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from scrapy.utils.project import get_project_settings
-
+from sse_starlette.sse import EventSourceResponse
 import google.generativeai as genai
 import json
 
@@ -38,8 +38,30 @@ def gemini_chat(data: dict):
     print('response:',response)
     return json.dumps(response)
 
+@app.post('/gemini/chat_stream', tags=["API"], summary="GEMINI")
+async def chat_stream(request: Request):
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    print('chat_stream json_post_raw:',json_post)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+
+    results=call_chat_stream(prompt)
 
 
+    return EventSourceResponse(results)
+
+async def call_chat_stream(prompt):
+    genai.configure(api_key="AIzaSyDO6L-EuN0Nkd2XlhEMTp9O3ERUSQ2vWNg")
+    model = genai.GenerativeModel('gemini-pro')
+    chat = model.start_chat(history=[])
+    response = chat.send_message(prompt, stream=True)
+    for chunk in response:
+        text=chunk.text
+        response = {"content": text}
+        print('response:',response)
+        yield json.dumps(response)
+        
 
 async def request(session, url):
     async with session.get(url) as response:
